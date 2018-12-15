6723c2e347514d05b754c5a253ca29af32b84688
Sriram Neelakantan <sriramneelakantan@Srirams-MacBook-Air-4.local>
2018-12-15 13:07:24 +0530

    Method Example

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el:'#vue-app',
   data:{
-    name:'Neel'
+    name:'Neel',
+    department:'UI'
+  },
+  methods:{
+    greetUser:function () {
+      return 'Good Morning';
+    }
   }
 })
