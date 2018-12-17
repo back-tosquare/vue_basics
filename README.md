1267a1142d2393a86cbd27cd568b2f21fcb89255
Sriram Neelakantan <sriramneelakantan@Srirams-MacBook-Air-4.local>
2018-12-17 10:28:19 +0530

    Computed Properties

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # vue_basics
-A Simple Repository illustrating 2-Way Data Binding
+A Simple Repository illustrating Computed Properties
 
-1. v-model
+1. Use Computed instead of methods
+2. While calling the Method don't use Parenthesis
+
+Eg : computed :{
+
+  method_A:function(){
+
+  },
+
+  method_B:function(){
+
+  }
+}
+
+The Value is = {{ method_A }}
--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 new Vue({
   el:'#vue-app',
   data:{
-    name:"",
-    age:""
+    age:20,
+    a:0,
+    b:0
   },
   methods:{
 
+    incA:function() {
+      this.a++;
+    },
+
+    incB:function() {
+      this.b++;
+    }
+    
+  },
+  computed:{
+
+    addToA:function() {
+      console.log('Add to A is Running')
+      return this.age + this.a;
+    },
+
+    addToB:function() {
+      console.log('Add to B is Running')
+      return this.age + this.b;
+    }
+
   }
+
 })
